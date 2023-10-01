398acbc27ba480957f8f0233d3fe87c2ec3b3452
Felipe Ferreira <55847931+FelipeFFerreira@users.noreply.github.com>
2023-10-01 19:25:52 -0300

    Improve readability for test-specific variables

--- femtorv32/FemtoRV/Frontend/riscv-tests/base_testbench/file_base/testbench.v
+++ femtorv32/FemtoRV/Frontend/riscv-tests/base_testbench/file_base/testbench.v
@@ -38,46 +38,46 @@ module flash_spi(
 	reg [7:0] recv_send;
 	reg [31:0]			MEM[`NRV_RAM:0];
 	reg [31:0]			rcv_data_dut;
-	wire [31:0] 		word_data_dut;
-	reg [5:0]			cnt_rcv = 32;
-	reg [5:0]			cnt_snd = 0;
-	reg [31:0]			cmd_addr_dut;
+	wire [31:0] 		flash_spi_data_to_sent;
+	reg [5:0]			flash_cnt_rcv = 32;
+	reg [5:0]			flash_cnt_snd = 0;
+	reg [31:0]			flash_cmd_addr_dut;
 	reg 				sds;
 	// wire       			receiving_dut = (rcv_bitcount_dut != 0);
 	// wire       			sending_dut   = (snd_bitcount_dut != 0);
-	assign  			spi_miso  = cmd_addr_dut[31];
-   	wire [32 - 1:0] 	addr_dut;
-   	assign 				addr_dut = {rcv_data_dut[19:0]};
-   	wire [31:0] 		mem2;
-   	assign mem2 =  		(cnt_rcv != 0) ? 32'hzzzz : 
-   						{ MEM[addr_dut[17 - 1:2]] };
+	assign  			spi_miso  = flash_cmd_addr_dut[31];
+   	wire [32 - 1:0] 	flash_requested_address;
+   	assign 				flash_requested_address = {rcv_data_dut[19:0]};
+   	wire [31:0] 		flash_current_instruction;
+   	assign flash_current_instruction =  		(flash_cnt_rcv != 0) ? 32'hzzzz : 
+   						{ MEM[flash_requested_address[17 - 1:2]] };
 
-	assign word_data_dut = {mem2[7:0], mem2[15:8], mem2[23:16], mem2[31:24]};
+	assign flash_spi_data_to_sent = {flash_current_instruction[7:0], flash_current_instruction[15:8], flash_current_instruction[23:16], flash_current_instruction[31:24]};
 	
 
 	always @(posedge spi_clk) begin
 		if (!reset_spi) begin
-			cnt_rcv = 32;
-			cnt_snd = 0;
+			flash_cnt_rcv = 32;
+			flash_cnt_snd = 0;
 		end else
 		if (SS == 0) begin
-			if (cnt_rcv >= 1 && cnt_snd === 0) begin
-				cnt_rcv <= cnt_rcv - 1;
+			if (flash_cnt_rcv >= 1 && flash_cnt_snd === 0) begin
+				flash_cnt_rcv <= flash_cnt_rcv - 1;
 				rcv_data_dut <= {rcv_data_dut[30:0], spi_mosi};
 				sds = 0;
 		end
-		else if (cnt_rcv == 0 && sds == 0) begin
-			cmd_addr_dut <= word_data_dut;
-			cnt_snd <= 32;
+		else if (flash_cnt_rcv == 0 && sds == 0) begin
+			flash_cmd_addr_dut <= flash_spi_data_to_sent;
+			flash_cnt_snd <= 32;
 			sds = 1;
 		end
-		else if (cnt_snd >= 1) begin
-			cnt_snd <= cnt_snd - 1;
-			cmd_addr_dut <= {cmd_addr_dut[30:0],1'b1};
+		else if (flash_cnt_snd >= 1) begin
+			flash_cnt_snd <= flash_cnt_snd - 1;
+			flash_cmd_addr_dut <= {flash_cmd_addr_dut[30:0],1'b1};
 		end
 		end 
-		if (cnt_snd == 1) cnt_rcv <= 32;
-		if (mem2 == 32'h00002137) en <= 1;
+		if (flash_cnt_snd == 1) flash_cnt_rcv <= 32;
+		if (flash_current_instruction == 32'h00002137) en <= 1;
 	end
 	
 		led_blink 
@@ -709,7 +709,7 @@ endmodule
 module testbench;
 
 	// Simulation time: 10000 * 1 us = 10 ms
-	localparam DURATION = 3000000;
+	localparam DURATION = 5000000;
 
 
 	reg clk, reset, reset_spi;
