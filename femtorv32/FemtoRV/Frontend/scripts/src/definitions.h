c9ef21bfca3564a3e14eb9fbd67d75fe6adaae02
Felipe Ferreira <55847931+FelipeFFerreira@users.noreply.github.com>
2023-10-06 00:09:22 -0300

    Enhanced terminal outputs with formatted text and colors. Translated all user-facing messages from Portuguese to English. Improved visual appeal and readability of program feedback.

--- femtorv32/FemtoRV/Frontend/scripts/src/definitions.h
+++ femtorv32/FemtoRV/Frontend/scripts/src/definitions.h
@@ -2,12 +2,14 @@
 #define _definitions_h
 
 /*Definiçoes dos repositorios de testes disponiveis*/
-#define NUM_OP                 4
+#define NUM_OP                 5
 
 #define COMPLIANCE_RISCV_ORG   1
 #define RISCV_SUITE_LOWRISCV   2
-#define UNIT_TESTS             3
-#define PERIPHERAL             4
+#define PERIPHERAL             3
+#define UNIT_TESTS             4
+#define GENERAL_PROGRAMS       5
+
 
 #define REPO_COMPLIANCE_RISCV_ORG   "riscv-tests"
 #define REPO_RISCV_SUITE_LOWRISCV   "riscv-test-suite"
--- femtorv32/FemtoRV/Frontend/scripts/src/script.c
+++ femtorv32/FemtoRV/Frontend/scripts/src/script.c
@@ -24,11 +24,15 @@ void Loading();
 
 int main() {
 
-    system("clear");  
+    system("clear"); 
+    // Header
+    printf("\033[1;36m-----------------------------------------\n");
+    printf("          Setup Environment\n");
+    printf("-----------------------------------------\033[0m\n\n"); 
 #ifdef ENV_FRONTEND_SIGNOFF
-    fprintf(stdout, "[%s] \n\nEnvironment For [SIMULATION] Configured\n", __func__);
+    printf("\033[1;32m[%s]\033[0m \n\nEnvironment For \033[1;34m[SIMULATION]\033[0m Configured\n", __func__);
 #else
-    fprintf(stdout, "[%s] \n\nEnvironment For [DEVICE] Configured\n", __func__);
+    printf("\033[1;32m[%s]\033[0m \n\nEnvironment For \033[1;34m[DEVICE]\033[0m Configured\n", __func__);
 #endif
 
     char* scriptfilePath_tooltchain = "../../";
@@ -40,81 +44,101 @@ int main() {
 
     Command(scriptfilePath_tooltchain, "make clean", true);
 
-    fprintf(stdout, "[%s] Installing TOOLCHAIN-RISCV - 32 bits\n", __func__);
+    fprintf(stdout, "\033[1;32m[%s] Installing TOOLCHAIN-RISCV - 32 bits\033[0m\n", __func__);
 
     Command(scriptfilePath_tooltchain, "make toolchain", true);
 
-    fprintf(stdout, "[%s] Processing Files For Compilation\n", __func__);
+    fprintf(stdout, "\033[1;34m[%s] Processing Files For Compilation\033[0m\n", __func__);
     
     char type_test[100];
 
 #ifdef ENV_FRONTEND_SIGNOFF
 
     switch (op) {
+        case COMPLIANCE_RISCV_ORG:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=riscv-tests", true);
+            printf("\033[1;34m>>COMPLIANCE_RISCV_ORG\033[0m\n");
+            Loading();
+            Command(scriptfilePath_tooltchain, "make riscv-tests TESTDIR=riscv-tests ENV_FRONTEND_SIGNOFF=1", true);
+            strcpy(type_test, "riscv-tests");
+            break;
+        
+        case RISCV_SUITE_LOWRISCV:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=riscv-test-suite", true);
+            printf("\033[1;34m>>RISCV_SUITE_LOWRISCV\033[0m\n");
+            Loading();
+            Command(scriptfilePath_tooltchain, "make riscv-test-suite TESTDIR=riscv-test-suite ENV_FRONTEND_SIGNOFF=1", true);
+            strcpy(type_test, "riscv-test-suite");
+            break;
 
-    case COMPLIANCE_RISCV_ORG:
-        Command(scriptfilePath_tooltchain, "make clean TESTDIR=riscv-tests", true);
-        printf(">>COMPLIANCE_RISCV_ORG\n");
-        Loading();
-        Command(scriptfilePath_tooltchain, "make riscv-tests TESTDIR=riscv-tests ENV_FRONTEND_SIGNOFF=1", true);
-        strcpy(type_test, "riscv-tests");
-        break;
-    
-    case RISCV_SUITE_LOWRISCV:
-        Command(scriptfilePath_tooltchain, "make clean TESTDIR=riscv-test-suite", true);
-        printf(">>RISCV_SUITE_LOWRISCV\n");
-        Loading();
-        Command(scriptfilePath_tooltchain, "make riscv-test-suite TESTDIR=riscv-test-suite ENV_FRONTEND_SIGNOFF=1", true);
-        strcpy(type_test, "riscv-test-suite");
-        break;
+        case PERIPHERAL:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
+            Loading();
+            printf("\033[1;34m>>TESTS_FOR_PERIPHERALS\033[0m\n");
+            Command(scriptfilePath_tooltchain, "make peripheral-tests TESTDIR=peripheral-tests ENV_FRONTEND_SIGNOFF=1", true);
+            break;
 
-    case UNIT_TESTS:
-        Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
-        Loading();
-        printf(">>UNIT_TESTS\n");
-        Command(scriptfilePath_tooltchain, "make peripheral-tests TESTDIR=peripheral-tests ENV_FRONTEND_SIGNOFF=1", true);
-        break;
+        case UNIT_TESTS:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
+            Loading();
+            printf("\033[1;34m>>UNIT_TESTS\033[0m\n");
+            Command(scriptfilePath_tooltchain, "make peripheral-tests TESTDIR=peripheral-tests", true);
+            strcpy(type_test, "peripheral-tests");
+            break;
 
-    case PERIPHERAL:
-        Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
-        Loading();
-        printf(">>PERIPHERAL\n");
-        Command(scriptfilePath_tooltchain, "make peripheral-tests TESTDIR=peripheral-tests", true);
-        strcpy(type_test, "peripheral-tests");
-        break;
-    
+        case GENERAL_PROGRAMS:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
+            Loading();
+            printf("\033[1;34m>>GENERAL_PROGRAMS\033[0m\n");
+            printf("\033[1;33m>Currently only supported for devices that use external flash. Go to the directory 'Program_Test'\033[0m\n");
+            exit(2);
+            break;
+        
         default:
-        printf( ">>Only the tools have been installed. Operation Canceled, because the informed test repository is invalid.\n!" );
-        exit(10);
+            printf("\033[1;31m>Only the tools have been installed. Operation Canceled, because the provided test repository is invalid!\033[0m\n");
+            exit(10);
         } 
 #else
         switch (op) {
 
-        case COMPLIANCE_RISCV_ORG:
-            Command(scriptfilePath_tooltchain, "make clean TESTDIR=riscv-tests", true);
-            printf(">>COMPLIANCE_RISCV_ORG\n");
-            Loading();
-            Command(scriptfilePath_tooltchain, "make riscv-tests TESTDIR=riscv-tests", true);
-            break;
-        
-        case RISCV_SUITE_LOWRISCV:
-            Loading();
-            printf(">Não Suportado\n");
-            exit(10);
-            break;
-        
-        case UNIT_TESTS:
-            printf(">>UNIT_TESTS\n");
-            break;
-
+            case COMPLIANCE_RISCV_ORG:
+                Command(scriptfilePath_tooltchain, "make clean TESTDIR=riscv-tests", true);
+                printf(">>COMPLIANCE_RISCV_ORG\n");
+                Loading();
+                Command(scriptfilePath_tooltchain, "make riscv-tests TESTDIR=riscv-tests", true);
+                break;
+            
+            case RISCV_SUITE_LOWRISCV:
+                Loading();
+                printf(">Não Suportado\n");
+                exit(10);
+                break;
+            
         case PERIPHERAL:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
             Loading();
-            printf(">Não Suportado\n");
-            exit(10);
+            printf("\033[1;34m>>TESTS_FOR_PERIPHERALS\033[0m\n");
+            Command(scriptfilePath_tooltchain, "make peripheral-tests TESTDIR=peripheral-tests ENV_FRONTEND_SIGNOFF=1", true);
             break;
 
-         default:
-            printf( ">>Only the tools have been installed. Operation Canceled, because the informed test repository is invalid.\n!" );
+        case UNIT_TESTS:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
+            Loading();
+            printf("\033[1;34m>>UNIT_TESTS\033[0m\n");
+            Command(scriptfilePath_tooltchain, "make peripheral-tests TESTDIR=peripheral-tests", true);
+            strcpy(type_test, "peripheral-tests");
+            break;
+
+        case GENERAL_PROGRAMS:
+            Command(scriptfilePath_tooltchain, "make clean TESTDIR=peripheral-tests", true);
+            Loading();
+            printf("\033[1;34m>>GENERAL_PROGRAMS\033[0m\n");
+            printf("\033[1;33m>Currently only supported for devices that use external flash. Go to the directory 'Program_Test'\033[0m\n");
+            exit(2);
+            break;
+        
+        default:
+            printf("\033[1;31m>Only the tools have been installed. Operation Canceled, because the provided test repository is invalid!\033[0m\n");
             exit(10);
         } 
 #endif
@@ -122,7 +146,7 @@ int main() {
     Command(scriptfilePath_tooltchain, "make hex", true);
 
     // Automating Tools
-    fprintf(stdout, "[%s] Compiling tools\n", __func__);
+    fprintf(stdout, "\033[1;34m[%s] Compiling tools\033[0m\n", __func__);
 
     Command(scriptfilePath_scripts, "make run-tools-elf", true);
 
@@ -130,7 +154,7 @@ int main() {
     
     Command(scriptfilePath_scripts, "make run-vvp", true);
 
-    fprintf(stdout, "[%s] Preparando arquivos para formatação\n", __func__);
+    fprintf(stdout, "\033[1;34m[%s] Preparing files for formatting\033[0m\n", __func__);
 
     MachineCodeTool();
     
@@ -138,14 +162,18 @@ int main() {
     GeneratSimulation(type_test);
 #endif
 
-    fprintf(stdout, "[%s] Gerando arquivos binarios de cada teste\n", __func__);
+    fprintf(stdout, "\033[1;34m[%s] Generating binary files for each test\033[0m\n", __func__);
 
     Command(scriptfilePath_tooltchain, "make bin", true);
 
 #ifndef ENV_FRONTEND_SIGNOFF
-    fprintf(stdout, "[%s] Preparando simulações para o device\n", __func__);
+    fprintf(stdout, "\033[1;34m[%s] Preparing simulations for the device\033[0m\n", __func__);
     GeneratSimulationDevice(); 
 #endif
+
+    printf("\n\033[1;36m-----------------------------------------\n");
+    printf("            End of Process\n");
+    printf("-----------------------------------------\033[0m\n");
 
     return 0;
 }
@@ -166,12 +194,12 @@ void Command(char * scriptfilePath, char * Command, bool make)
         wait(&status);
 
         if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-            printf("Makefile concluído com sucesso!\n");
+            printf("\033[1;32mMakefile completed successfully!\033[0m\n");
         } else {
-            printf("Erro na execução do Makefile.\n");
+            printf("\033[1;31mError during Makefile execution.\033[0m\n");
         }
     } else {
-        printf("Erro ao executar o comando 'make programa'.\n");
+        printf("\033[1;31mError executing the 'make programa' command.\033[0m\n");
     }
 }
 
@@ -180,7 +208,7 @@ void MachineCodeTool() {
 
     DIR* directory = opendir(directory_path);
     if (directory == NULL) {
-        perror("Erro ao abrir o diretório");
+        perror("Error opening the directory");
         return 1;
     }
 
@@ -205,7 +233,7 @@ void MachineCodeTool() {
 void GeneratSimulation(char *type_test)
 {
     char command[100];
-    fprintf(stdout, "[%s] Preparando arquivos para simulação\n", __func__);
+    printf("\033[1;32m[%s]\033[0m Preparing files for simulation\n", __func__);
     snprintf(command, sizeof(command), "./run-vvp %s", type_test);
     Command("", command, false);
 }
@@ -216,7 +244,7 @@ void GeneratSimulationDevice()
 
     DIR* directory = opendir(directory_path);
     if (directory == NULL) {
-        perror("Erro ao abrir o diretório");
+        perror("Error opening the directory");
         return 1;
     }
 
@@ -225,13 +253,13 @@ void GeneratSimulationDevice()
     while ((entry = readdir(directory)) != NULL) {
         if (entry->d_type == DT_DIR) {
             if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-                printf("[%s] Executando simulação do teste [%s] no Device:\n", __func__, entry->d_name);
+                printf("[%s] Running simulation test [%s] on Device:\n", __func__, entry->d_name);
                 char dir_path[100], command[100];
                 snprintf(dir_path, sizeof(dir_path), "%s%s", directory_path, entry->d_name);
                 snprintf(command, sizeof(command), "sudo icesprog -w -o 0x00020000 %s.bin", entry->d_name);
-                printf("path: %s | comando: %s\n", dir_path, command);
+                printf("\033[1;34mpath:\033[0m %s \033[1;34m|\033[0m \033[1;34mcommand:\033[0m %s\n\033[0m", dir_path, command);
                 Command(dir_path, command, true);
-                printf("Aguardando ...\n");
+                printf("\033[1;33mWaiting...\033[0m\n");
                 sleep(5);
             }
         }
@@ -243,27 +271,47 @@ void GeneratSimulationDevice()
 int DisplayLoad(char * opcao[NUM_OP]) 
 {
     int i, op;
-    printf("\n[ Choose the desired repository for designing and running the tests ]\n");
-    for (i = 0; i < NUM_OP; i++) {
-        printf("%-30s\t\t\tOP:%d\n", opcao[i], i + 1);
+    int width = 50;
+    printf("\n");
+    printf("\033[1;34m"); 
+    for (i = 0; i < width; i++) {
+        printf("=");
     }
-    printf("\n[Choose an option]: ");
+    printf("\n\033[0m"); 
+
+    int padding = (width - strlen("[ Choose the desired repository for designing and running the tests ]")) / 2;
+    printf("\033[1;34m=\033[0m");
+    for (i = 0; i < padding - 1; i++) printf(" ");
+    printf("\033[1;32m[ Choose the desired repository for designing and running the tests ]\033[0m");
+    for (i = 0; i < padding - 1; i++) printf(" ");
+    printf("\033[1;34m=\033[0m\n");
+
+    // Opções
+    for (i = 0; i < NUM_OP; i++) {
+        printf("\033[1;34m=\033[0m \033[1;36m%-30s\033[0m\t\t\t\033[1;31mOP:%d\033[0m \033[1;34m=\033[0m\n", opcao[i], i + 1);
+    }
+    printf("\033[1;34m"); 
+    for (i = 0; i < width; i++) {
+        printf("=");
+    }
+    printf("\033[0m\n"); 
+    printf("\n\033[1;37m[Choose an option]: \033[0m");
     scanf(" %d", &op);
     return op;
 }
 
 int Display()
 {
-    char * opcoes[NUM_OP] = { "RISC-V compliance tests",
+    char * opcoes[NUM_OP] = { "RISC-V compliance tests[©]\t",
                               "lowRISC compliance tests",
                               "tests for peripherals", 
+                              "unit tests",
                               "general programs",
                             };
     int op;
 
     return op = DisplayLoad(opcoes);
 }
-
 void Loading()
 {
     #define BAR_LENGTH 50 
@@ -282,6 +330,5 @@ void Loading()
         
         usleep(5000000/BAR_LENGTH);
     }
-    
     printf("\n");
 }
